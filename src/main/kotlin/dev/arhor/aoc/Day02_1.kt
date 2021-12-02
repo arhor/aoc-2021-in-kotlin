e1aecfe933a7b5b57cee53131c175f1d837b50da
Maksim Buryshynets <maksim.buryshynets@ukg.com>
2021-12-02 16:08:12 +0300

    Day 02 - puzzles 1 and 2 implementation shorten/simplified

--- src/main/kotlin/dev/arhor/aoc/Day02_1.kt
+++ src/main/kotlin/dev/arhor/aoc/Day02_1.kt
@@ -1,19 +1,17 @@
 package dev.arhor.aoc
 
 fun main() {
-    operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>) = (first + other.first) to (second + other.second)
-
-    fun handleCommand(command: String, value: Int): Pair<Int, Int> = when (command) {
-        "forward" -> value to 0
-        "up"      -> 0 to -value
-        "down"    -> 0 to value
-        else      -> 0 to 0
-    }
-
     val result = ResourceReader.readInput("/Day02_1.txt") { lines ->
         val (position, depth) = lines.map { it.split(" ") }
-            .map { (command, value) -> handleCommand(command, value.toInt()) }
-            .reduce { prev, next -> prev + next }
+            .map { (command, value) -> command to value.toInt() }
+            .fold(Pair(0, 0)) { (position, depth), (command, value) ->
+                when (command) {
+                    "up"      -> position to (depth - value)
+                    "down"    -> position to (depth + value)
+                    "forward" -> (position + value) to depth
+                    else      -> position to depth
+                }
+            }
 
         position * depth
     }
--- src/main/kotlin/dev/arhor/aoc/Day02_2.kt
+++ src/main/kotlin/dev/arhor/aoc/Day02_2.kt
@@ -2,20 +2,16 @@ package dev.arhor.aoc
 
 fun main() {
     val result = ResourceReader.readInput("/Day02_1.txt") { lines ->
-        var position = 0
-        var depth = 0
-        var aim = 0
-
-        lines.map { it.split(" ") }.forEach { (command, value) ->
-            when (command) {
-                "up"      -> aim -= value.toInt()
-                "down"    -> aim += value.toInt()
-                "forward" -> {
-                    position += value.toInt()
-                    depth += aim * value.toInt()
+        val (position, depth) = lines.map { it.split(" ") }
+            .map { (command, value) -> command to value.toInt() }
+            .fold(Triple(0, 0, 0)) { (position, depth, aim), (command, value) ->
+                when (command) {
+                    "up"      -> Triple(position, depth, (aim - value))
+                    "down"    -> Triple(position, depth, (aim + value))
+                    "forward" -> Triple((position + value), (depth + (aim * value)), aim)
+                    else      -> Triple(position, depth, aim)
                 }
             }
-        }
         position * depth
     }
     println("result: $result")
