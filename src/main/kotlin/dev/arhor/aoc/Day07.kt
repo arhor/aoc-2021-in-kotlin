89a2d71ecee0578596962c1ce0cae82eb9d5860d
Maksim Buryshynets <maksim.buryshynets@ukg.com>
2021-12-09 16:55:11 +0300

    Day 07 - puzzle 1 and 2 complete

---
+++ src/main/kotlin/dev/arhor/aoc/Day07.kt
@@ -0,0 +1,28 @@
+package dev.arhor.aoc
+
+import dev.arhor.aoc.ResourceReader.readInput
+import kotlin.math.abs
+
+fun main() {
+    fun solvePuzzleGeneric(input: Sequence<String>, calcCost: (steps: Int) -> Int): Int {
+        val crabs = input.first().split(",").map(String::toInt)
+        val statistics = crabs.stream().mapToInt { it }.summaryStatistics()
+        val cache = HashMap<Int, Int>()
+        val sums = ArrayList<Int>()
+        for (position in statistics.min..statistics.max) {
+            var sum = 0
+            for (crab in crabs) {
+                sum += cache.computeIfAbsent(abs(position - crab)) { calcCost(it) }
+            }
+            sums.add(sum)
+        }
+        return sums.minOrNull() ?: 0
+    }
+
+    fun solvePuzzle1(input: Sequence<String>) = solvePuzzleGeneric(input) { steps -> steps }
+
+    fun solvePuzzle2(input: Sequence<String>) = solvePuzzleGeneric(input) { steps -> (0..steps).sum() }
+
+    println("result 1: ${readInput("/Day07.txt", ::solvePuzzle1)}")
+    println("result 2: ${readInput("/Day07.txt", ::solvePuzzle2)}")
+}
