353e3283089a632e60bd25c6cffbe7a199a3d27a
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-11-18 11:53:56 +0300

    * refactoring;

--- src/main/kotlin/dev/arhor/aoc/Day11.kt
+++ src/main/kotlin/dev/arhor/aoc/Day11.kt
@@ -17,46 +17,7 @@ fun main() {
 class OctopusesStateModel(input: Sequence<String>) : MatrixModel(input) {
 
     var counter = 0
-
-    fun incrementEnergy() {
-        for (row in data) {
-            for (index in row.indices) {
-                row[index]++
-            }
-        }
-    }
-
-    fun resetEnergy() {
-        for (row in data) {
-            for ((index, value) in row.withIndex()) {
-                if (value < 0 || value > 9) {
-                    row[index] = 0
-                }
-            }
-        }
-    }
-
-    fun findEmpoweredOctopuses(): List<Point> {
-        val points = ArrayList<Point>()
-        for ((row, line) in data.withIndex()) {
-            for ((col, energy) in line.withIndex()) {
-                if (energy > 9) {
-                    points += Point(col, row)
-                }
-            }
-        }
-        return points
-    }
-
-    fun flash(point: Point) {
-        data[point.y][point.x] = -1
-        counter++
-        findAdjacentCoordinates(point.x, point.y, data, diagonal = true).forEach {
-            if (data[it] != -1) {
-                data[it.y][it.x]++
-            }
-        }
-    }
+        private set
 
     fun simulate(steps: Int) {
         repeat(steps) {
@@ -70,6 +31,46 @@ class OctopusesStateModel(input: Sequence<String>) : MatrixModel(input) {
             }
 
             resetEnergy()
+        }
+    }
+
+    private fun incrementEnergy() {
+        for (row in data) {
+            for (index in row.indices) {
+                row[index]++
+            }
+        }
+    }
+
+    private fun resetEnergy() {
+        for (row in data) {
+            for ((index, value) in row.withIndex()) {
+                if (value < 0 || value > 9) {
+                    row[index] = 0
+                }
+            }
+        }
+    }
+
+    private fun findEmpoweredOctopuses(): List<Point> {
+        val points = ArrayList<Point>()
+        for ((row, line) in data.withIndex()) {
+            for ((col, energy) in line.withIndex()) {
+                if (energy > 9) {
+                    points += Point(col, row)
+                }
+            }
+        }
+        return points
+    }
+
+    private fun flash(point: Point) {
+        data[point.y][point.x] = -1
+        counter++
+        findAdjacentCoordinates(point.x, point.y, data, diagonal = true).forEach {
+            if (data[it] != -1) {
+                data[it.y][it.x]++
+            }
         }
     }
 }
