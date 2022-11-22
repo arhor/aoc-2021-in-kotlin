a72434e4d2ea3e7c72c012326dde1bbd7a9d14a5
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-11-22 10:54:27 +0300

    day 13 - added template with test data

---
+++ src/main/kotlin/dev/arhor/aoc/Day13.kt
@@ -0,0 +1,9 @@
+package dev.arhor.aoc
+
+import dev.arhor.aoc.ResourceReader.readInput
+
+fun main() {
+    fun solvePuzzle1(input: Sequence<String>) = ""
+
+    println("result 1: ${readInput("/Day13.txt", ::solvePuzzle1)}")
+}
