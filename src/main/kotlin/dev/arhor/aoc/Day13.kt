be4b748c863c1c6b8f798f48a86807e36e14f09f
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-11-23 20:04:06 +0300

    day 13 - extremely brood, but working solution, to be refactored;

--- src/main/kotlin/dev/arhor/aoc/Day13.kt
+++ src/main/kotlin/dev/arhor/aoc/Day13.kt
@@ -1,52 +1,157 @@
 package dev.arhor.aoc
 
 import dev.arhor.aoc.ResourceReader.readInput
+import kotlin.math.max
 
 private val COORDINATES_PATTERN = Regex("([0-9]+),([0-9]+)")
 private val INSTRUCTION_PATTERN = Regex("fold along ([xy])=([0-9]+)")
 
 fun main() {
-    fun solvePuzzle1(input: Sequence<String>) = DataModel(input)
+    fun solvePuzzle1(input: Sequence<String>) = DataModel(input).calculateVisiblePoints()
 
     println("result 1: ${readInput("/Day13.txt", ::solvePuzzle1)}")
 }
 
-private enum class Coordinate { X, Y }
+private sealed interface Fold {
+    @JvmInline
+    value class X(val value: Int) : Fold
 
-private data class Folding(val coordinate: Coordinate, val value: Int)
+    @JvmInline
+    value class Y(val value: Int) : Fold
 
-@Suppress("VARIABLE_WITH_REDUNDANT_INITIALIZER")
+    companion object {
+        fun by(name: String, value: String) = when (name) {
+            "x" -> X(value.toInt())
+            "y" -> Y(value.toInt())
+            else -> throw IllegalArgumentException("Unsupported coordinate: '$name'")
+        }
+    }
+}
+
 private class DataModel(input: Sequence<String>) {
 
-    private val coordinates: List<Point>
-    private val instruction: List<Folding>
+    private val data: Array<IntArray>
+
+    private val points: List<Point>
+    private val instructions: List<Fold>
 
     init {
-        val tempCoordinates = ArrayList<Point>()
-        val tempInstruction = ArrayList<Folding>()
+        val tempPoints = ArrayList<Point>().also { points = it }
+        val tempInstructions = ArrayList<Fold>().also { instructions = it }
+
         for (line in input) {
             run { // allows to simulate 'continue' inside the 'let' function
                 COORDINATES_PATTERN.find(line)?.let { result ->
-                    tempCoordinates += result.destructured.let {
+                    tempPoints += result.destructured.let { (x, y) ->
                         Point(
-                            x = it.component1().toInt(),
-                            y = it.component2().toInt(),
+                            x = x.toInt(),
+                            y = y.toInt(),
                         )
                     }
                     return@run // 'continue' alternative to use inside the 'let' function
                 }
                 INSTRUCTION_PATTERN.find(line)?.let { result ->
-                    tempInstruction += result.destructured.let {
-                        Folding(
-                            coordinate = Coordinate.valueOf(it.component1().uppercase()),
-                            value = it.component2().toInt(),
-                        )
+                    tempInstructions += result.destructured.let { (name, value) ->
+                        Fold.by(name, value)
                     }
                     return@run // 'continue' alternative to use inside the 'let' function
                 }
             }
         }
-        coordinates = tempCoordinates
-        instruction = tempInstruction
+
+        data = Array(size = points.maxOf { it.y } + 1) {
+            IntArray(size = points.maxOf { it.x } + 1)
+        }
+
+        for (point in points) {
+            data[point.y][point.x] = 1
+        }
+    }
+
+    fun calculateVisiblePoints(): Int {
+        var array: Array<IntArray> = data
+
+        for (instruction in instructions) {
+            val result = ArrayList<IntArray>()
+
+            when (instruction) {
+                is Fold.X -> {
+                    for (it in array) {
+                        val one: MutableList<Int?> = it.slice(0 until instruction.value).toMutableList()
+                        val two: MutableList<Int?> = it.slice(instruction.value + 1 until it.size).reversed().toMutableList()
+
+                        val diff = one.size - two.size
+                        if (diff > 0) {
+                            repeat(diff) {
+                                two.add(0, null)
+                            }
+                        } else if (diff < 0) {
+                            repeat(-diff) {
+                                one.add(0, null)
+                            }
+                        }
+
+                        result += IntArray(max(one.size, two.size)) { i ->
+                            (one[i] ?: 0) or (two[i] ?: 0)
+                        }
+                    }
+                }
+
+                is Fold.Y -> {
+                    val one: MutableList<IntArray?> = array.slice(0 until instruction.value).toMutableList()
+                    val two: MutableList<IntArray?> = array.slice(instruction.value + 1 until array.size).reversed().toMutableList()
+
+                    val diff = one.size - two.size
+
+                    if (diff > 0) {
+                        repeat(diff) {
+                            two.add(0, null)
+                        }
+                    } else if (diff < 0) {
+                        repeat(-diff) {
+                            one.add(0, null)
+                        }
+                    }
+
+                    val max = one.mapNotNull { it?.size }.max()
+
+                    for ((index, line) in one.withIndex()) {
+                        result += IntArray(max) { i ->
+                            (line?.get(i) ?: 0) or (two[index]?.get(i) ?: 0)
+                        }
+                    }
+                }
+            }
+            array = result.toTypedArray()
+        }
+
+        println(
+            array.joinToString(separator = "\n") { line ->
+                line.joinToString(separator = "") { point ->
+                    if (point >= 1) {
+                        "#"
+                    } else {
+                        "."
+                    }
+                }
+            }
+        )
+
+        return array.fold(0) { acc, points -> acc + points.sum() }
     }
 }
+/*
+.##....##.#..#..##..####.#..#.#..#.#..#
+#..#....#.#..#.#..#....#.#..#.#.#..#..#
+#.......#.####.#..#...#..####.##...#..#
+#.......#.#..#.####..#...#..#.#.#..#..#
+#..#.#..#.#..#.#..#.#....#..#.#.#..#..#
+.##...##..#..#.#..#.####.#..#.#..#..##.
+---------------------------------------
+##.#...##.#..#.####.####.#..#.#.##.#..#.
+#..#....#.####.#..#...##.####.###..#..#.
+#.......#.##.#.####..#...#.##.###..#..#.
+#..#.#..#.#..#.####.##...#..#.#.#..#..#.
+####.##.#.#..#.#..#.####.#..#.#..#.###..
+
+ */
