f8c2d1f1f11d01e1a7812df45ee0e1b239cc2890
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-11-24 19:47:00 +0300

    day 14 - initial solution;

---
+++ src/main/kotlin/dev/arhor/aoc/Day14.kt
@@ -0,0 +1,47 @@
+package dev.arhor.aoc
+
+import dev.arhor.aoc.ResourceReader.readInput
+
+private val INSERTION_RULE_PATTERN = Regex("^([A-Z]{2}) -> ([A-Z])$")
+
+fun main() {
+    fun solvePuzzle1(input: Sequence<String>): Any? {
+        var template = ""
+        val rules = HashMap<String, String>()
+        for ((index, line) in input.withIndex()) {
+            when (index) {
+                0 -> {
+                    template = line
+                }
+
+                1 -> {
+                    continue
+                }
+
+                else -> {
+                    INSERTION_RULE_PATTERN.findAll(line).map { it.destructured }.forEach { (pair, insertion) ->
+                        rules[pair] = insertion
+                    }
+                }
+            }
+        }
+        repeat(10) {
+            template = template.windowed(2).withIndex().joinToString(separator = "") { (i, pair) ->
+                val rule = rules[pair]
+
+                if (rule != null) {
+                    if (i == 0) {
+                        pair.first() + rule + pair.last()
+                    } else {
+                        rule + pair.last()
+                    }
+                } else {
+                    pair
+                }
+            }
+        }
+        return template.groupingBy { it }.eachCount().values.let { it.max() - it.min() }
+    }
+
+    println("result 1: ${readInput("/Day14.txt", ::solvePuzzle1)}")
+}
